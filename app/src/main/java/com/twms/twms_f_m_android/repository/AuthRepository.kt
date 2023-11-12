babdb6e61d9497e6f9a136a059ee44e8d36d86a0
Tótok Patrik <totokpatrik@gmail.com>
2023-11-12 18:03:34 +0100

    initial config

--- app/src/main/java/com/twms/twms_f_m_android/repository/AuthRepository.kt
+++ app/src/main/java/com/twms/twms_f_m_android/repository/AuthRepository.kt
@@ -1,13 +1,10 @@
 package com.twms.twms_f_m_android.repository
 
-import androidx.paging.PagingData
 import com.twms.twms_f_m_android.data.model.Auth
 import com.twms.twms_f_m_android.data.model.AuthResponse
 import com.twms.twms_f_m_android.data.model.CustomResponse
 import com.twms.twms_f_m_android.util.Resource
 import kotlinx.coroutines.flow.Flow
-import javax.inject.Inject
-import javax.inject.Singleton
 
 
 interface AuthRepository {
--- app/src/main/java/com/twms/twms_f_m_android/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/twms/twms_f_m_android/ui/menu/MenuFragment.kt
@@ -1,32 +1,34 @@
 package com.twms.twms_f_m_android.ui.menu
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.twms.twms_f_m_android.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.twms.twms_f_m_android.databinding.FragmentMenuBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MenuFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MenuFragment()
-    }
+    private lateinit var binding: FragmentMenuBinding
 
-    private lateinit var viewModel: MenuViewModel
+    private lateinit var navController: NavController
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+    ): View {
+        binding = FragmentMenuBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MenuViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
 
+    }
 }
